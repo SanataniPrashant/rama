404003944e4f1c877f7139befa6c3f94ae6cae72
Prashant Pandey <prashantpandey76867@gmail.com>
2024-02-11 11:11:29 +0530

    remark

---
+++ mult.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{ int num1;
+  int num2;
+  cout <<"Enter the first number : ";
+cin >> num1;
+cout <<"Enter the second number : ";
+cin >> num2;
+cout << num1<<"-" << num2 << "="<< num1 * num2 << endl;
+return 0;
+}
